beec9f73c341108a118497999b4e508a2364eb0b
kunalhk2029 <kunalhk2029plane@gmail.com>
2023-05-18 16:58:25 +0530

    Added ExpressionEvaluator.kt

---
+++ app/src/test/java/com/app/calculator/domain/ExpressionEvaluatorTest.kt
@@ -0,0 +1,92 @@
+package com.app.calculator.domain
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class ExpressionEvaluatorTest{
+    private lateinit var evaluator: ExpressionEvaluator
+
+    @Test
+    fun `Simple expression properly evaluated`() {
+        evaluator = ExpressionEvaluator(
+            listOf(
+                ExpressionPart.Number(4.0),
+                ExpressionPart.Op(Operation.ADD),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Op(Operation.SUBTRACT),
+                ExpressionPart.Number(3.0),
+                ExpressionPart.Op(Operation.MULTIPLY),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Op(Operation.DIVIDE),
+                ExpressionPart.Number(3.0),
+            )
+        )
+        assertThat(evaluator.evaluate()).isEqualTo(4.00)
+    }
+
+    @Test
+    fun `Expression with decimals properly evaluated`() {
+        evaluator = ExpressionEvaluator(
+            listOf(
+                ExpressionPart.Number(4.5),
+                ExpressionPart.Op(Operation.ADD),
+                ExpressionPart.Number(5.5),
+                ExpressionPart.Op(Operation.SUBTRACT),
+                ExpressionPart.Number(3.5),
+                ExpressionPart.Op(Operation.MULTIPLY),
+                ExpressionPart.Number(5.5),
+                ExpressionPart.Op(Operation.DIVIDE),
+                ExpressionPart.Number(3.5),
+            )
+        )
+        assertThat(evaluator.evaluate()).isEqualTo(4.5)
+    }
+
+    @Test
+    fun `Simple equation with parentheses properly evaluated`() {
+        evaluator = ExpressionEvaluator(
+            listOf(
+                ExpressionPart.Number(4.0),
+                ExpressionPart.Op(Operation.ADD),
+                ExpressionPart.Parentheses(ParenthesesType.Opening),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Op(Operation.SUBTRACT),
+                ExpressionPart.Number(3.0),
+                ExpressionPart.Parentheses(ParenthesesType.Closing),
+                ExpressionPart.Op(Operation.MULTIPLY),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Op(Operation.DIVIDE),
+                ExpressionPart.Number(4.0),
+            )
+        )
+        assertThat(evaluator.evaluate()).isEqualTo(6.5)
+    }
+
+    @Test
+    fun `Simple equation with nested parentheses properly evaluated`() {
+        evaluator = ExpressionEvaluator(
+            listOf(
+                ExpressionPart.Number(4.43),
+                ExpressionPart.Op(Operation.SUBTRACT),
+                ExpressionPart.Parentheses(ParenthesesType.Opening),
+                ExpressionPart.Number(4.0),
+                ExpressionPart.Op(Operation.MULTIPLY),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Op(Operation.SUBTRACT),
+                ExpressionPart.Parentheses(ParenthesesType.Opening),
+                ExpressionPart.Number(2.0),
+                ExpressionPart.Op(Operation.ADD),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Op(Operation.SUBTRACT),
+                ExpressionPart.Parentheses(ParenthesesType.Opening),
+                ExpressionPart.Number(3.0),
+                ExpressionPart.Op(Operation.MULTIPLY),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Parentheses(ParenthesesType.Closing),
+                ExpressionPart.Parentheses(ParenthesesType.Closing),
+                ExpressionPart.Parentheses(ParenthesesType.Closing)
+            )
+        )
+        assertThat(evaluator.evaluate()).isEqualTo(-23.57)
+    }
+}
